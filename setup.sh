8b6381c7dba6e2622443517634ee522e0e0a988d
AlisonSalerno <60582447+AlisonSalerno@users.noreply.github.com>
2020-07-07 16:59:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,9 @@ echo "\
 [general]\n\
 email = \"salernoali@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+
