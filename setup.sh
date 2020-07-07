c12a3c755a7e65cb694ee90556970a573bc3cb4d
AlisonSalerno <60582447+AlisonSalerno@users.noreply.github.com>
2020-07-07 17:17:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"salernoali@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
 
